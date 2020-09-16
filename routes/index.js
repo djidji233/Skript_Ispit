eb25f8af87309900764782fa568ece4be8515be4
Djordje Zivanovic <djolezile@gmail.com>
2020-09-16 15:29:52 +0200

    added some stuff

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router()
 // Login (Main) page
 // GET /
 router.get('/', (req, res) => {
-    res.render('login')
+    res.render('login', {
+        layout: 'login'
+    })
 })
 
 
