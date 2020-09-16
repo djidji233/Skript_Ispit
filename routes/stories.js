d20b51bd51da1fb4f7b78bd1c6223827dd9206b3
Djordje Zivanovic <djolezile@gmail.com>
2020-09-16 22:35:26 +0200

    Add Story done

--- routes/stories.js
+++ routes/stories.js
@@ -11,6 +11,20 @@ router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
 
+// Process add form
+// POST /stories
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
+
+
 
 
 module.exports = router
